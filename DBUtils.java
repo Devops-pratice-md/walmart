6a30c61f7a3e621c8ef470da46ab4d25ace6ca36
ManideepPasavala <pmanideep8@gmail.com>
2022-05-11 20:41:54 -0400

    deployment msg

--- DBUtils.java
+++ DBUtils.java
@@ -1,2 +1,4 @@
+hello I am updating in Dev branch..
 this file cotains java code
 jsahkdas
+
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-this file cintains the deployment scripts
+this file contains the deployment scripts....
+file has been edited on 12th may
 
